cf6b6224a7c98958fdc8ee7305a66a7c15e313ef
Mats G <m.gaupset@gmail.com>
2024-02-05 22:20:42 +0100

    Made boilerplate index page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,10 +1,31 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
-import NavbarComp from "./NavbarComp.tsx";
+import { Col, Container, Row } from 'react-bootstrap';
+import CardComp from "./CardComp";
+import FooterComp from "./FooterComp";
+import NavbarComp from "./NavbarComp";
 
 function App() {
 
   return (
-    <NavbarComp></NavbarComp>
+    <>
+      <Container fluid> 
+        <NavbarComp></NavbarComp>
+        <Container className='mt-4'>
+          <Row className="justify-content-md-center">
+            <Col>
+              <CardComp></CardComp>
+            </Col>
+            <Col>
+              <CardComp></CardComp>
+            </Col>
+            <Col>
+              <CardComp></CardComp>
+            </Col>
+          </Row>
+        </Container>
+        <FooterComp></FooterComp>
+      </Container>
+    </>
   )
 }
 
