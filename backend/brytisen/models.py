cae81805dd65953b8d5d30f1d216a2c9a6a80a75
Snorre Saether <snosaet@hotmail.com>
2024-02-08 11:53:07 +0100

    #6 added activity model

--- backend/brytisen/models.py
+++ backend/brytisen/models.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 
 from django.db import models
+from django.conf import settings
 
 # Create your models here.
 
@@ -8,9 +9,43 @@ from django.db import models
 class Activity(models.Model):
     """This is a (data)model, it can be views as an object."""
 
-    activity_name =     models.CharField(
+    name = models.CharField(
         max_length=120)  # Aktivitet navn: f.eks. "Topptur"
 
-    activity_details = models.TextField()  # Utdypende beskrivelse av aktivitet
+    details = models.TextField()  # Utdypende beskrivelse av aktivitet
 
-    # TODO: add more fields for the Activity object
+    activity_rules = models.TextField(default="Default rules apply.")
+
+    # TODO: consider adding image field
+
+    UNDEFINED = "UNDEFINED"
+    TRENING = "TRENING"
+    FYLLA = "FYLLA"
+    VERV = "VERV"
+    TOPPTUR = "TOPPTUR"
+
+    TYPE_CHOICES = [
+        # TODO: make serius 
+        (UNDEFINED, 'Undefined'),
+        (TRENING, 'Trening'),
+        (FYLLA, 'Fylla'),
+        (VERV, 'Verv'),
+        (TOPPTUR, 'Topptur'),
+    ]
+
+    activity_type = models.CharField(
+        max_length=120,
+        choices=TYPE_CHOICES,
+        default=UNDEFINED,
+   )
+
+    owner = models.ForeignKey(settings.AUTH_USER_MODEL,
+                              on_delete = models.CASCADE,
+                              related_name='activites',
+                              null = True, #  Temporarily allow null
+                              )
+    isReported = models.BooleanField(null = True)
+    
+
+
+# TODO: add review class
