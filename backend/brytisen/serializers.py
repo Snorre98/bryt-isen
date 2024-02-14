b4fd496beb85f924ecc97e51223ae3fafe3938e1
Snorre Saether <snosaet@hotmail.com>
2024-02-14 21:57:50 +0100

    #10 adds login, logout and register user functionality

--- backend/brytisen/serializers.py
+++ backend/brytisen/serializers.py
@@ -1,13 +1,130 @@
 from __future__ import annotations
 
+import itertools
+
+from guardian.models import UserObjectPermission
+
 from rest_framework import serializers
 
-from .models import Activity
+from django.contrib.auth import authenticate
+from django.contrib.auth.models import Permission
+
+from .models import User, Activity
 
 
 class ActivitySerializer(serializers.ModelSerializer):
     class Meta:
         model = Activity
-        #fields = ('name', 'activity_description')
+        # fields = ('name', 'activity_description')
         #  TODO: add more fields to the model in models, then here
         fields = '__all__'
+
+
+##
+## User and user-autentication related serializers
+##
+class LoginSerializer(serializers.Serializer):
+    """
+    Autenticates username and password
+    Credit to: https://www.guguweb.com/2022/01/23/django-rest-framework-authentication-the-easy-way/
+    This uses Django Rest Framework to autenticate
+    """
+
+    username = serializers.CharField(label='Username', write_only=True)
+    password = serializers.CharField(
+        label='Password',
+        ## Showing
+        style={'input_type': 'password'},
+        trim_whitespace=False,
+        write_only=True,
+    )
+
+    """
+    selfe: the instance of the object, like "this"
+    responseAttributes: a JSON object containing data from a HTTP request
+        JSON can be parsed as a dict in Python    
+    """
+
+    def validate(self, responseAttributes: dict) -> dict:
+        username = responseAttributes.get('username')
+        password = responseAttributes.get('password')
+
+        if username and password:
+            user = authenticate(request=self.context.get('request'), username=username, password=password)  # Django auth magic
+            if not user:
+                msg = 'user not registered.'
+                raise serializers.ValidationError(msg, code='autorization')
+            else:
+                msg = 'Both username and password are required.'
+                raise serializers.ValidationError(msg, code='authorization')
+            responseAttributes['user'] = user
+            return responseAttributes
+
+
+class RegisterSerializer(serializers.Serializer):
+    """
+    Serializer for user registration.
+    """
+
+    username = serializers.CharField(label='Username', write_only=True)
+    first_name = serializers.CharField(label='First Name', write_only=True)
+    last_name = serializers.CharField(label='Last Name', write_only=True)
+    password = serializers.CharField(
+        label='Password',
+        style={'input_type': 'password'},
+        trim_whitespace=False,
+        write_only=True,
+    )
+
+    def validate(self, data):
+        """Validates and creates a new user."""
+
+        username = data.get('username')
+        first_name = data.get('first_name')
+        last_name = data.get('last_name')
+        password = data.get('password')
+
+        if not (username and password):
+            raise serializers.ValidationError('Username and password are required.')
+
+        # Attempt to create a new user
+        try:
+            user = User.objects.create_user(username=username, first_name=first_name, last_name=last_name, password=password)
+        except Exception as e:
+            raise serializers.ValidationError(f'Error creating user: {str(e)}')
+
+        data['user'] = user
+        return data
+
+
+class UserSerializer(serializers.ModelSerializer):
+    # TODO: Create get_permissions, get_object_permissions
+    permissions = serializers.SerializerMethodField(method_name='get_permissions', read_only=True)
+    object_permissions = serializers.SerializerMethodField(method_name='get_object_permissions', read_only=True)
+
+    class Meta:
+        model = User
+        exclude = ['password', 'user_permissions']  # does not serialise these fields
+
+    def get_permissions(self, user: User) -> list[str]:
+        return user.get_all_permissions()  # Django permissions magic
+
+    @staticmethod
+    def _permission_to_str(permission: Permission) -> str:
+        return f'{permission.content_type.app_label}.{permission.codename}'  # Django permissions magic
+
+    def _obj_permission_to_obj(self, obj_perm: UserObjectPermission) -> dict[str, str]:
+        perm_obj = {
+            'obj_ok': obj_perm.object_pk,
+            'permission': self._permission_to_str(permission=obj_perm.permission),
+        }
+        return perm_obj  # more Django (guardian) permissions magic
+
+    def get_object_permissions(self, user: User) -> list[dict[str, str]]:
+        user_object_perms_qs = UserObjectPermission.object.filter(user=user)  #  findes appropriat perms
+
+        perm_objs = []
+        for obj_perm in itertools.chain(user_object_perms_qs):
+            perm_objs.append(self._obj_permission_to_obj(obj_perm=obj_perm))
+
+        return perm_objs  # list of objects which user has permissions to manipulate
--- backend/brytisen/views.py
+++ backend/brytisen/views.py
@@ -1,18 +1,20 @@
 from __future__ import annotations
 
-from rest_framework import viewsets
+from rest_framework import status, viewsets
 from rest_framework.views import APIView
 from rest_framework.request import Request
+from rest_framework.generics import ListAPIView
 from rest_framework.response import Response
-from rest_framework.permissions import AllowAny, BasePermission
+from rest_framework.permissions import AllowAny, BasePermission, IsAuthenticated, DjangoModelPermissionsOrAnonReadOnly
 
-from django.shortcuts import render
+# from django.shortcuts import render
+from django.contrib.auth import login, logout
 from django.middleware.csrf import get_token
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_protect, ensure_csrf_cookie
 
-from .models import Activity
-from .serializers import ActivitySerializer
+from .models import User, Activity
+from .serializers import UserSerializer, LoginSerializer, ActivitySerializer, RegisterSerializer
 
 # Create your views here.
 
@@ -20,6 +22,71 @@ from .serializers import ActivitySerializer
 class ActivityView(viewsets.ModelViewSet):
     serializer_class = ActivitySerializer
     queryset = Activity.objects.all()
+
+
+@method_decorator(csrf_protect, 'dispatch')
+class LoginView(APIView):
+    permission_classes = [AllowAny]  # Django Rest Framework (not pure Django) auth system
+
+    def post(self, request: Request):
+        serializer = LoginSerializer(data=self.request.data, context={'request': self.request})  # The LoginSerializer authenticates password and username
+        serializer.is_valid(raise_exception=True)  # only goes forward if login is validated
+        user = serializer.validate_data['user']
+        login(request=request, user=user, backend='django.contrib.auth.backends.ModelBackend')
+        new_csrf_token = get_token(request=request)
+
+        # creates response (JSON) to completed login, with HTTP response code and token in header:
+        response = Response(status=status.HTTP_200_ACCEPTED, data=new_csrf_token, headers={'X-CSRFToken': new_csrf_token})
+
+        return response
+
+
+@method_decorator(csrf_protect, 'dispatch')
+class LogoutView(APIView):
+    permission_classes = [IsAuthenticated]  # Django REST framework authentication category
+
+    def post(self, request: Request) -> Response:
+        if not request.user.is_autenticated:
+            return Response(status=status.HTTP_400_BAD_REQUEST)
+        logout(request)
+        response = Response(status=status.HTTP_200_OK)  # if statment above did not catch, this code runs, successfully loging out user
+
+        return response
+
+
+@method_decorator(csrf_protect, 'dispatch')
+class RegisterView(APIView):
+    permission_classes = [AllowAny]
+
+    def post(self, request: Request) -> Response:
+        serializer = RegisterSerializer(data=self.request.data, context={'request': self.request})
+        serializer.is_valid(raise_exception=True)
+        user = serializer.validated_data['user']  # validate registration
+
+        # login after register
+        login(request=request, user=user, backend='django.contrib.auth.backends.ModelBackend')
+        new_csrf_token = get_token(request=request)
+
+        # Create regiser respons
+        response = Response(
+            status=status.HTTP_202_ACCEPTED,
+            data=new_csrf_token,
+            headers={'X-CSRFToken': new_csrf_token},
+        )
+        return response
+
+
+class UserView(APIView):
+    parser_classes = [IsAuthenticated]
+
+    def get(self, request: Request) -> Response:
+        return Response(data=UserSerializer(request.user, many=False).data)
+
+
+class AllUsersView(ListAPIView):
+    permission_classes = (DjangoModelPermissionsOrAnonReadOnly,)
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
 
 
 @method_decorator(ensure_csrf_cookie, 'dispatch')
