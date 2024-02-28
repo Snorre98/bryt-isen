6d80a1ac9a1af0272e6f3912c6c91dcc62a9e7aa
Anders Vestrum <andevest@ntnu.no>
2024-02-28 12:09:52 +0100

    Made button in detailComp that opens a reviewForm
    Co-authored-by: eirik kvam

---
+++ frontend/src/components/ReviewForm.tsx
@@ -0,0 +1,21 @@
+import { title } from "process";
+import { Modal, Button } from "react-bootstrap";
+
+export type ReviewFormProps = {
+  rating: number;
+  review: string;
+};
+
+export default function ReviewForm() {
+  
+    
+
+
+
+  return (
+    <div>
+      <h2>Review</h2>
+      <p>Rating</p>
+    </div> // Add closing tag for the div element
+  );
+}
