60668b42262c6d42a697f5ffb3ba411216f49eba
Mats Gaupset <matsgau@stud.ntnu.no>
2024-03-13 11:31:43 +0100

    Final final favoritt funksjonalitet

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from 'react';
-import { getActivities } from '~/api';
+import { getActivities, getFavoritedActivities } from '~/api';
 import CardComp from '~/components/CardComp';
 import { ActivityDto } from '../dto';
 import '../styles/Home.css';
@@ -8,6 +8,8 @@ import { Icon } from '@iconify/react';
 import FilterComponent from '~/components/FilterComponent/FilterComponent';
 import { useGlobalContext } from '~/contextProviders/GlobalContextProvider';
 import { CustomToast } from '~/components/CustomToast';
+import Form from 'react-bootstrap/Form';
+import { useAuthContext } from '~/contextProviders/AuthContextProvider';
 
 export function Home() {
   const [activities, setActivities] = useState<ActivityDto[]>([]);
@@ -16,19 +18,44 @@ export function Home() {
   const [loading, setLoading] = useState(true);
   const [showErrorToast, setShowErrorToast] = useState(false);
   const [errorToastMessage, setErrorToastMessage] = useState('');
+  const [showFavorites, setShowFavorites] = useState(false); // New state for checkbox
+  const { user, setUser } = useAuthContext();
 
   useEffect(() => {
     getActivities()
       .then((data) => {
         setLoading(false);
-        setActivities(data);
+        if(showFavorites) {
+          getFavoritedActivities().then(favoriteActivities => {
+            // Assuming favoriteActivities is an array of objects { activity_id, owner }
+            // and data is an array of activity objects with properties { id, owner }
+        
+            // Filter activities based on matching `id` and `owner` with `activity_id` and `owner` from favoriteActivities
+            const filteredActivities = data.filter(activity =>
+              favoriteActivities.some(favActivity =>
+                favActivity.activity_id == activity.id && favActivity.owner === activity.owner && activity.owner == user?.id
+              )
+            );
+        
+            setActivities(filteredActivities);
+          }).catch(error => {
+            console.error("Could not fetch favorite activities", error);
+            // Handle errors or set error state here
+          });
+        } else {
+          setActivities(data);
+        }
       })
       .catch((error) => {
         setShowErrorToast(true);
         setErrorToastMessage('Kunne ikke hente inn aktiviteter!');
         console.log(error);
       });
-  }, []);
+  }, [showFavorites]);
+
+  const handleShowFavoritesChange = (event: any) => {
+    setShowFavorites(event.target.checked);
+  };
 
   return (
     <PageWrapper>
@@ -53,6 +80,20 @@ export function Home() {
           justifyContent: 'center',
         }}
       >
+        <div>
+
+        <Form.Check // prettier-ignore
+          type="switch"
+          id="custom-switch"
+          label="Vis kun favoritter"
+          onChange={handleShowFavoritesChange} // Add the onChange handler here
+          checked={showFavorites} // Control the checked state
+        />
+
+        <br />
+
+
+        </div>
         <div className="activityCardWrapper">
           {activities.length > 0 &&
             (isFilterOn
